5ca77edd131bcde27fb46f92452a91f4f71dc237
Talha <syedtalha5314@gmail.com>
2019-04-17 21:18:41 +0530

    Output Not As Expected

--- prgrm29.c
+++ prgrm29.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int xpow(int, int);
+#include <math.h>
 int armstrong_number(int);
 
 int main(void)
 {
-    long int i = 1;
+    int i = 1;
     for (i; i < 10000; i++)
     {
         armstrong_number(i);
@@ -25,7 +25,7 @@ int armstrong_number(int x)
     while (y > 0)
     {
         hold = y % 10;
-        temp = temp + (xpow(hold, digit_power));
+        temp = temp + (pow(hold, digit_power));
         y = y / 10;
     }
 
@@ -35,12 +35,4 @@ int armstrong_number(int x)
     }
 }
 
-int xpow(int a, int b)
-{
-    int i = 2;
-    while (i <= b)
-    {
-        a *= a;
-    }
-    return a;
 }
