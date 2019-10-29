097a0d00af10902be5f39477487dc10e966faf5e
symtalha14 <syedtalha5314@gmail.com>
2019-10-29 12:41:53 +0530

    Merge Sort , sorting in linear time

--- Sorting/merge_sort.c
+++ Sorting/merge_sort.c
@@ -1,8 +1,8 @@
-//Merge Sort
+//mergeSort Sort
 #include<stdio.h>
 
-int mergeSort(int arr[] , int lower , int upper);
-int merge(int arr[] , int lower , int mid , int upper);
+int splitArr(int arr[] , int lower , int upper);
+int mergeSort(int arr[] , int lower , int mid , int upper);
 
 
 int main(){
@@ -12,33 +12,34 @@ int main(){
     lower=0;
     upper = (sizeof(ages)/sizeof(int))-1;
     //mid =(int)(lower+upper)/2;
-    mergeSort(ages,lower,upper);
-    for(lower=0;lower<upper;lower++){
+    splitArr(ages,lower,upper);
+    for(lower=0;lower<=upper;lower++){
         printf("%d ",ages[lower]);
     }
 }
 
 
-int mergeSort(int arr[] , int lower ,int upper){
+int splitArr(int arr[] , int lower ,int upper){
     //condition to check multi-elements array
     if(lower<upper){
 
-        int mid =(int)lower+(upper-1)/2;
-        mergeSort(arr , lower , mid);//first half of the array
-        mergeSort(arr , mid+1 , upper);//second half of the array
-        //merge the arrays after splitting
-        merge(arr,lower,mid,upper);
+        int mid =(lower+upper)/2;
+        splitArr(arr , lower , mid);//first half of the array
+        splitArr(arr , mid+1 , upper);//second half of the array
+        //merge & Sort the arrays after splitting
+        mergeSort(arr,lower,mid,upper);
     }
 }
 //[1,2,3,4,5] 
-int merge(int arr[] , int lower, int mid , int upper){
-    int L = lower + mid + 1;
+int mergeSort(int arr[] , int lower, int mid , int upper){
+    int L = mid-lower+ 1;
     int R = upper - mid;
     int left_half[L] , right_half[R] ,i=0,j=0,k=lower;
 
     //filling the left half
     while(i<L){
         left_half[i]=arr[k];
+   
         i++;
         k++;
     }
@@ -50,7 +51,7 @@ int merge(int arr[] , int lower, int mid , int upper){
     }
 
     //sort the elements in arr by comparing L and R
-    k=lower;
+    k=lower;i=0;j=0;
     while(i<L && j<R){
         if(left_half[i]<=right_half[j]){
             arr[k]=left_half[i];
@@ -62,12 +63,12 @@ int merge(int arr[] , int lower, int mid , int upper){
         k++;
     }
     while(i<L){
-        arr[k]=arr[i];
+        arr[k]=left_half[i];
         i++;
         k++;
     }
     while(j<R){
-        arr[k]=arr[j];
+        arr[k]=right_half[j];
         j++;
         k++;
     }
